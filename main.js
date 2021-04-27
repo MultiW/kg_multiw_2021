bcc4846a591dc45f7ba8f5d6ab52b9fa81ab35bb
Xin Wang <xinwang.wang@outlook.com>
2021-04-26 19:54:15 -0700

    Implemented integer to words algorithm (#1)
    * Implemented the int to words algorithm as a JavaScript CLI tool.
    * Removed unnecessary .gitignore
    * Create README.md

---
+++ main.js
@@ -0,0 +1,64 @@
+/**
+ * Convert a list of whole numbers into an array of strings representing the phonetic equivalent of 
+ * the whole numbers.
+ * @param {number[]|string[]} intList List of **whole numbers**
+ * @returns {string[]} List of words corresponding to integers in intList
+ */
+function intToWords(intList) {
+    const digitMap = {
+        "0": "Zero",
+        "1": "One",
+        "2": "Two",
+        "3": "Three",
+        "4": "Four",
+        "5": "Five",
+        "6": "Six",
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine"
+    };
+
+    wordsList = [];
+
+    // iterate each whole number
+    intList.forEach(intStr => {
+        let word = ""
+
+        // iterate each digit of the whole number
+        for (let i = 0; i < intStr.length; i++) {
+            let digit = intStr.charAt(i);
+
+            if (!(digit in digitMap)) {
+                throw `Unexpected character "${digit}" in input. Expected whole numbers.`;
+            }
+
+            word += digitMap[digit];
+        }
+
+        wordsList.push(word);
+    });
+
+    return wordsList;
+}
+
+function main() {
+    // Get int list from CLI arguments
+    let intList = [];
+    process.argv.forEach((arg, index) => {
+        // Skip unrelated arguments, i.e. "node main.js"
+        if (index < 2) {
+            return
+        }
+
+        intList.push(arg);
+    })
+
+    // Convert to word list
+    let wordsList = intToWords(intList);
+
+    // Format word list for display
+    console.log(wordsList.join(","))
+}
+
+// == Script Start ==
+main()
